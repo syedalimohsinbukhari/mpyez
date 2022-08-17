5f17419efe4f6dd6eee814b2e93a7cda698140c5
noonu <astrophysicsandpython@gmail.com>
2022-08-17 16:38:42 +0500

    [MINOR-28]
    * added Sort_ function to dict_.py
    * changed some stuff in read_files.py

--- src/mpyez/dict_.py
+++ src/mpyez/dict_.py
@@ -1,1 +1,15 @@
 """Created on Jul 23 04:26:54 2022."""
+
+
+class Sort_:
+
+    def __init__(self, input_dictionary, sort_by='values'):
+        self.inp_dict = input_dictionary
+        self.sort_ = sort_by
+
+    def __sorter(self, ind, reverse):
+        # taken from https://stackoverflow.com/a/613218/3212945
+        return dict(sorted(self.inp_dict.items(), key=lambda x: x[ind], reverse=reverse))
+
+    def get(self, reverse=False):
+        return self.__sorter(1 if self.sort_ == 'values' else 0, reverse=reverse)
--- src/mpyez/read_files.py
+++ src/mpyez/read_files.py
@@ -1,20 +1,21 @@
 """Created on Jul 23 16:56:48 2022."""
 
-
-def read_text_file_in_a_list(file_to_read):
-    open_file = [value.readlines() for _, value in enumerate(open(file=file_to_read, mode='r'))]
-
-    return [line.split('\n')[0] for line in open_file[0]]
+from src.mpyez.utilities.read_files.utilities import check_for_errors
 
 
-def read_specific_lines_from_a_text_file(file_to_read, lines_to_read):
-    out = read_text_file_in_a_list(file_to_read=file_to_read)
+def read_txt_file(file_to_read) -> list:
+    open_file = [value for _, value in enumerate(open(file=file_to_read, mode='r'))]
+
+    return [line.strip('\n') for line in open_file]
+
+
+def get_lines_from_txt_file(file_to_read, lines_to_read) -> list:
+    out = read_txt_file(file_to_read=file_to_read)
 
     if not isinstance(lines_to_read, list):
         lines_to_read = [lines_to_read]
 
-    misc.check_for_errors(open_file=out, lines_to_read=lines_to_read)
+    check_for_errors(open_file=out, lines_to_read=lines_to_read)
 
     # taken from https://stackoverflow.com/a/2082131/3212945
-    return [value.split('\n')[0] for index, value in enumerate(open(file=file_to_read, mode='r')) if
-            index in set(lines_to_read)]
+    return [value for index, value in enumerate(out) if index in set(lines_to_read)]
