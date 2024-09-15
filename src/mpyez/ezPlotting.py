d0ac78bd2d7c13046d489f391934c502a3f62b78
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-09-15 19:32:01 +0500

    [0003-crash-development2-minor-2-1]
    1. minor improvements

--- src/mpyez/ezPlotting.py
+++ src/mpyez/ezPlotting.py
@@ -15,7 +15,7 @@ import numpy as np
 
 
 def plot_two_column_file(file_name: str, delimiter: str = ',', skip_header: bool = False, auto_label: bool = False,
-                         fig_size: Tuple[int, int] = (12, 5), is_scatter: bool = False, axis=None) -> plt:
+                         fig_size: Tuple[int, int] = (12, 5), is_scatter: bool = False) -> plt:
     """
     Reads a two-column file (x, y) and plots the data.
 
@@ -36,8 +36,6 @@ def plot_two_column_file(file_name: str, delimiter: str = ',', skip_header: bool
         The size of the plot figure, only used if a new plot is created (default is (12, 5)).
     is_scatter : bool, optional
         If True, creates a scatter plot. Otherwise, creates a line plot (default is False).
-    axis : matplotlib axis, optional
-        A specific matplotlib axis object to plot on. If None, a new plot is created (default is None).
 
     Returns
     -------
@@ -51,11 +49,11 @@ def plot_two_column_file(file_name: str, delimiter: str = ',', skip_header: bool
 
     x_data, y_data = data.T
 
-    return plot_xy(x_data, y_data, auto_label, fig_size, is_scatter, axis)
+    return plot_with_dual_axes(x1_data=x_data, y1_data=y_data, auto_label=auto_label, fig_size=fig_size, is_scatter=is_scatter)
 
 
 def plot_xy(x_data: np.ndarray, y_data: np.ndarray, auto_label: bool = False, fig_size: Tuple[int, int] = (12, 5),
-            is_scatter: bool = False, axis=None) -> plt:
+            is_scatter: bool = False) -> plt:
     """
     Plots x_data against y_data with customizable options.
 
@@ -74,44 +72,20 @@ def plot_xy(x_data: np.ndarray, y_data: np.ndarray, auto_label: bool = False, fi
         The size of the plot figure (default is (12, 5)).
     is_scatter : bool, optional
         If True, creates a scatter plot. Otherwise, creates a line plot (default is False).
-    axis : matplotlib axis, optional
-        A specific matplotlib axis object to plot on. If None, a new plot is created (default is None).
 
     Returns
     -------
     plt : matplotlib.pyplot
         The matplotlib.pyplot object for the generated plot.
     """
-    plotter = axis if axis else plt
-    if not axis:
-        plotter.figure(figsize=fig_size)
-
-    if is_scatter:
-        plot_function = plotter.scatter
-    else:
-        plot_function = plotter.plot
-
-    plot_function(x_data, y_data)
-
-    if auto_label:
-        label_functions = {'x_label': plotter.set_xlabel if axis else plotter.xlabel,
-                           'y_label': plotter.set_ylabel if axis else plotter.ylabel,
-                           'title': plotter.set_title if axis else plotter.title}
-
-        label_functions['x_label']('X-axis')
-        label_functions['y_label']('Y-axis')
-        label_functions['title']('X vs Y Plot')
-        plotter.legend(loc='best')
-        plotter.tight_layout()
-
-    return plotter
+    return plot_with_dual_axes(x1_data=x_data, y1_data=y_data, auto_label=auto_label, fig_size=fig_size, is_scatter=is_scatter)
 
 
 def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
                         x2_data: Optional[np.ndarray] = None, y2_data: Optional[np.ndarray] = None,
                         x1y1_label: str = 'X1 vs Y1', x1y2_label: str = 'X1 vs Y2', x2y1_label: str = 'X2 vs Y1',
-                        is_dual_y: bool = False, auto_label: bool = False, fig_size: Tuple[int, int] = (12, 5),
-                        is_scatter: bool = False, color_y2: str = 'red', legend_loc: str = 'best') -> plt:
+                        use_twin_x: bool = False, auto_label: bool = False, fig_size: Tuple[int, int] = (12, 5),
+                        is_scatter: bool = False, color_y2: str = 'red') -> plt:
     """
     Plots data with options for dual axes (x or y) or single axis.
 
@@ -131,7 +105,7 @@ def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
         Label for the plot of X1 vs Y2 (when using dual Y-axes). Default is 'X1 vs Y2'.
     x2y1_label : str, optional
         Label for the plot of X2 vs Y1 (when using dual X-axes). Default is 'X2 vs Y1'.
-    is_dual_y : bool, optional
+    use_twin_x : bool, optional
         If True, creates dual y-axis plot. If False, creates dual x-axis plot. Default is False.
     auto_label : bool, optional
         If True, automatically labels the axes and plot title. Default is False.
@@ -141,8 +115,6 @@ def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
         If True, creates scatter plot; otherwise, line plot. Default is False.
     color_y2 : str, optional
         Color for the secondary y-axis or X2 axis plot (default is 'red').
-    legend_loc : str, optional
-        Location of the legend (default is 'best').
 
     Returns
     -------
@@ -150,10 +122,10 @@ def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
         The matplotlib.pyplot object for the generated plot.
     [fig, (ax1, ax2)] or [fig, ax1] depending on the presence of dual axes.
     """
-    if is_dual_y and y2_data is None:
-        raise ValueError("Dual Y-axis plot requested but 'y2_data' is None.")
-    if not is_dual_y and x2_data is None:
-        raise ValueError("Dual X-axis plot requested but 'x2_data' is None.")
+    if use_twin_x and x2_data is not None:
+        raise ValueError("Dual Y-axis plot requested but 'x2_data' given.")
+    if not use_twin_x and y2_data is not None:
+        raise ValueError("Dual X-axis plot requested but 'y2_data' given.")
     if len(x1_data) == 0 or len(y1_data) == 0:
         raise ValueError("Primary x or y data is empty. Please provide valid data.")
 
@@ -164,17 +136,20 @@ def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
 
     _plot_or_scatter(ax1, is_scatter)(x1_data, y1_data, label=x1y1_label)
 
+    ax2 = None
+
     if auto_label:
         ax1.set_xlabel('X1')
         ax1.set_ylabel('Y1')
         ax1.set_title('Plot')
 
-    if is_dual_y:
+    if use_twin_x:
         ax2 = ax1.twinx()
         _plot_or_scatter(ax2, is_scatter)(x1_data, y2_data, label=x1y2_label, color=color_y2)
         if auto_label:
             ax2.set_ylabel('Y2')
-    else:
+
+    elif x2_data is not None:
         ax2 = ax1.twiny()
         _plot_or_scatter(ax2, is_scatter)(x2_data, y1_data, label=x2y1_label, color=color_y2)
         if auto_label:
@@ -186,7 +161,7 @@ def plot_with_dual_axes(x1_data: np.ndarray, y1_data: np.ndarray,
         handles += handles2
         labels += labels2
 
-    ax1.legend(handles, labels, loc=legend_loc)
+    ax1.legend(handles, labels, loc='best')
 
     plt.tight_layout()
 
