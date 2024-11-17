eccaaa78335435590deee853a8864759ba11b19f
Syed Ali Mohsin Bukhari <syedali.b@outlook.com>
2024-11-17 22:04:24 +0500

    [main-minor-33]
    1. updated `setup.py`, and `.tokeignore` files
    2. added `version.py` for versioning history
    3. updated `requirements.txt`, and created `environment.yaml`

--- setup.py
+++ setup.py
@@ -1,27 +1,40 @@
 """Setup file"""
 
+import os
+
 from setuptools import find_packages, setup
 
-with open('README.md', 'r') as f:
-    readme = f.read()
 
-setup(
-        name='mpyez',
-        version='0.0.7',
-        packages=find_packages(where="src", exclude=['*tests*']),
-        url='https://github.com/syedalimohsinbukhari/mpyez',
-        license='MIT',
-        author='Syed Ali Mohsin Bukhari',
-        author_email='syedali.b@outlook.com',
-        description='Common use backend for python done easy.',
-        long_description=readme,
-        long_description_content_type="text/markdown",
-        python_requires=">=3.9",
-        install_requires=["numpy==1.26.4", "matplotlib", "setuptools"],
-        include_package_data=True,
-        classifiers=["License :: OSI Approved :: MIT License",
-                     "Programming Language :: Python :: 3.9",
-                     "Programming Language :: Python :: 3.10",
-                     "Programming Language :: Python :: 3.11",
-                     "Programming Language :: Python :: 3.12"],
-)
+def load_metadata():
+    """get the metadata for the package."""
+    metadata = {}
+    with open(os.path.join("src", "mpyez", "version.py")) as f:
+        exec(f.read(), metadata)
+    return metadata
+
+
+metadata_ = load_metadata()
+
+with open('README.md', 'r') as readme_file:
+    readme = readme_file.read()
+
+setup(name='mpyez',
+      version=metadata_['__version__'],
+      author=metadata_['__author__'],
+      author_email=metadata_['__email__'],
+      license=metadata_['__license__'],
+      url=metadata_['__url__'],
+      description=metadata_['__description__'],
+      packages=find_packages(where="src", exclude=["test"]),
+      package_dir={"": "src"},
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      python_requires=">=3.9",
+      install_requires=["numpy==1.26.4", "matplotlib", "setuptools"],
+      include_package_data=True,
+      classifiers=["License :: OSI Approved :: MIT License",
+                   "Programming Language :: Python :: 3.9",
+                   "Programming Language :: Python :: 3.10",
+                   "Programming Language :: Python :: 3.11",
+                   "Programming Language :: Python :: 3.12"],
+      )
